6bfa5b727b5579bd12d56b5abd8b5a53e6484a7d
Sidharth chauhan <chauhansiddharth71@gmail.com>
2025-12-29 14:43:08 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  // This matches your repository name exactly
+  base: '/sidharth/', 
 })
