c8efb4d101e0dfeb4fe67efdfafd3f09f1c662a8
Sidharth chauhan <chauhansiddharth71@gmail.com>
2025-12-29 14:39:58 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Add this line to make links relative for hosting
+  base: './', 
 })
